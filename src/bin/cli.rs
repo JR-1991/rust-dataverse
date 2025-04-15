f833f80c7c2f62fdfea62b65f2db039a94eb68b1
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-04-16 01:20:33 +0200

    update cli

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
 
 use colored::Colorize;
+use dataverse::cli::admin::AdminSubCommand;
+use dataverse::cli::auth::{AuthProfile, AuthSubCommand};
 use structopt::StructOpt;
 
 use dataverse::cli::base::Matcher;
@@ -9,40 +11,84 @@ use dataverse::cli::dataset::DatasetSubCommand;
 use dataverse::cli::file::FileSubCommand;
 use dataverse::cli::info::InfoSubCommand;
 use dataverse::client::BaseClient;
+use dataverse::search_api::query::SearchQuery;
 
 static HEADER: &str = r#"
 --- Dataverse Command Line Interface (DVCLI) ---
 "#;
 
-// This is the basic overall structure of the CLI
-// Subcommands are defined in their respective modules
-// and are processed here.
+#[derive(StructOpt, Debug)]
+struct GlobalOpts {
+    /// Profile name to use for configuration
+    #[structopt(short, long)]
+    profile: Option<String>,
+}
+
 #[derive(StructOpt, Debug)]
 #[structopt(about = "CLI to interact with Dataverse")]
+#[allow(clippy::upper_case_acronyms)]
+struct CLI {
+    #[structopt(flatten)]
+    global: GlobalOpts,
+
+    #[structopt(subcommand)]
+    cmd: DVCLI,
+}
+
+#[derive(StructOpt, Debug)]
+#[allow(clippy::upper_case_acronyms)]
 enum DVCLI {
     Info(InfoSubCommand),
     Collection(CollectionSubCommand),
     Dataset(DatasetSubCommand),
     File(FileSubCommand),
+    Search(SearchQuery),
+    Admin(AdminSubCommand),
+    Auth(AuthSubCommand),
 }
 
 fn main() {
-    let client = setup_client().expect("Failed to set up client.");
-    let dvcli = DVCLI::from_args();
+    let cli = CLI::from_args();
+
+    // This is a special case for the Auth command, which is used to set the profile
+    // and does not require a Dataverse instance.
+    if let DVCLI::Auth(cmd) = cli.cmd {
+        let client = BaseClient::new("https://None", None).expect("Failed to create base client");
+        cmd.process(&client);
+        return;
+    }
+
+    let client = match cli.global.profile {
+        Some(profile) => setup_client_from_keyring(&profile).expect("Failed to set up client."),
+        None => setup_client_from_env().expect("Failed to set up client."),
+    };
 
     if atty::is(atty::Stream::Stdout) {
         println!("{}", HEADER.bold());
     }
 
-    match dvcli {
+    // You can now access the profile via cli.global.profile
+    match cli.cmd {
         DVCLI::Info(command) => command.process(&client),
         DVCLI::Collection(command) => command.process(&client),
         DVCLI::Dataset(command) => command.process(&client),
         DVCLI::File(command) => command.process(&client),
+        DVCLI::Search(command) => command.process(&client),
+        DVCLI::Admin(command) => command.process(&client),
+        DVCLI::Auth(command) => command.process(&client),
     }
 }
 
-fn setup_client() -> Result<BaseClient, Box<dyn Error>> {
+fn setup_client_from_keyring(name: &str) -> Result<BaseClient, Box<dyn Error>> {
+    let auth_profile = AuthProfile::get_from_keyring(name)?;
+    let client = BaseClient::new(
+        auth_profile.get_url(),
+        Some(&auth_profile.get_token().to_string()),
+    )?;
+    Ok(client)
+}
+
+fn setup_client_from_env() -> Result<BaseClient, Box<dyn Error>> {
     let (base_url, api_token) = extract_config_from_env();
     let client = BaseClient::new(&base_url, api_token.as_ref())?;
     Ok(client)
@@ -56,7 +102,7 @@ fn extract_config_from_env() -> (String, Option<String>) {
 
     // If there is no base URL, return None
     if base_url.is_none() {
-        panic!("No base URL provided. Please set the DVCLI_URL environment variable.");
+        panic!("Neither profile name nor base URL provided. Please set the DVCLI_URL environment variable or use a profile name with the --profile flag.");
     }
 
     (base_url.unwrap(), api_token)
