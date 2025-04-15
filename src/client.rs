669c0ca58d14d871c66da67e4d5a03bf0f8bbca9
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-04-16 01:19:46 +0200

    add `File` case

--- src/client.rs
+++ src/client.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 
 use atty::Stream;
 use colored::Colorize;
+use reqwest::header::HeaderMap;
+use reqwest::header::HeaderValue;
 use reqwest::Client;
 use reqwest::Url;
 use serde::Deserialize;
@@ -9,19 +11,36 @@ use serde::Deserialize;
 use crate::request::RequestType;
 use crate::response::Response;
 
+#[derive(Debug, Clone)]
 pub struct BaseClient {
     base_url: Url,
     api_token: Option<String>,
     client: Client,
 }
 
+impl BaseClient {
+    pub(crate) fn has_api_token(&self) -> bool {
+        self.api_token.is_some()
+    }
+}
+
 // This is the base client that will be used to make requests to the API.
 // Its acts as a wrapper around the reqwest::blocking::Client and provides
 // methods to make GET, POST, PUT, and DELETE requests.
 impl BaseClient {
-    pub fn new(base_url: &String, api_token: Option<&String>) -> Result<Self, reqwest::Error> {
+    pub fn new(base_url: &str, api_token: Option<&String>) -> Result<Self, reqwest::Error> {
         let base_url = Url::parse(base_url).unwrap();
-        let client = Client::new();
+        let default_headers = Self::default_headers(api_token);
+
+        // Create a client with increased timeouts for large file uploads
+        let client = Client::builder()
+            .timeout(std::time::Duration::from_secs(300)) // 5-minute timeout
+            .connect_timeout(std::time::Duration::from_secs(60)) // 1-minute connect timeout
+            .pool_idle_timeout(std::time::Duration::from_secs(90)) // Keep connections alive longer
+            .pool_max_idle_per_host(10)
+            .default_headers(default_headers)
+            .build()?;
+
         Ok(BaseClient {
             base_url,
             api_token: api_token.map(|s| s.to_owned().to_string()),
@@ -29,49 +48,93 @@ impl BaseClient {
         })
     }
 
+    fn default_headers(api_token: Option<&String>) -> HeaderMap {
+        let mut headers = HeaderMap::new();
+
+        if let Some(api_token) = api_token {
+            headers.insert(
+                "X-Dataverse-key",
+                api_token.parse().expect("Failed to parse API token"),
+            );
+        }
+
+        // Add the default headers
+        headers.insert("Connection", HeaderValue::from_static("keep-alive"));
+        headers.insert("Accept", HeaderValue::from_static("*/*"));
+        headers.insert("User-Agent", HeaderValue::from_static("dv-rs/0.1.0"));
+        headers.insert(
+            "Accept-Encoding",
+            HeaderValue::from_static("gzip, deflate, br, zstd"),
+        );
+
+        headers
+    }
+
     pub async fn get(
         &self,
         path: &str,
         parameters: Option<HashMap<String, String>>,
-        context: &RequestType,
+        context: RequestType,
+        header_map: Option<HeaderMap>,
     ) -> Result<reqwest::Response, reqwest::Error> {
-        self.perform_request(reqwest::Method::GET, path, parameters, context).await
+        self.perform_request(reqwest::Method::GET, path, parameters, context, header_map)
+            .await
     }
 
     pub async fn post(
         &self,
         path: &str,
         parameters: Option<HashMap<String, String>>,
-        context: &RequestType,
+        context: RequestType,
+        header_map: Option<HeaderMap>,
     ) -> Result<reqwest::Response, reqwest::Error> {
-        self.perform_request(reqwest::Method::POST, path, parameters, context).await
+        self.perform_request(reqwest::Method::POST, path, parameters, context, header_map)
+            .await
     }
 
     pub async fn put(
         &self,
         path: &str,
         parameters: Option<HashMap<String, String>>,
-        context: &RequestType,
+        context: RequestType,
+        header_map: Option<HeaderMap>,
     ) -> Result<reqwest::Response, reqwest::Error> {
-        self.perform_request(reqwest::Method::PUT, path, parameters, context).await
+        self.perform_request(reqwest::Method::PUT, path, parameters, context, header_map)
+            .await
     }
 
     pub async fn delete(
         &self,
         path: &str,
         parameters: Option<HashMap<String, String>>,
-        context: &RequestType,
+        context: RequestType,
+        header_map: Option<HeaderMap>,
     ) -> Result<reqwest::Response, reqwest::Error> {
-        self.perform_request(reqwest::Method::DELETE, path, parameters, context).await
+        self.perform_request(
+            reqwest::Method::DELETE,
+            path,
+            parameters,
+            context,
+            header_map,
+        )
+        .await
     }
 
     pub async fn patch(
         &self,
         path: &str,
         parameters: Option<HashMap<String, String>>,
-        context: &RequestType,
+        context: RequestType,
+        header_map: Option<HeaderMap>,
     ) -> Result<reqwest::Response, reqwest::Error> {
-        self.perform_request(reqwest::Method::PATCH, path, parameters, context).await
+        self.perform_request(
+            reqwest::Method::PATCH,
+            path,
+            parameters,
+            context,
+            header_map,
+        )
+        .await
     }
 
     async fn perform_request(
@@ -79,26 +142,40 @@ impl BaseClient {
         method: reqwest::Method,
         path: &str,
         parameters: Option<HashMap<String, String>>,
-        context: &RequestType,
+        context: RequestType,
+        header_map: Option<HeaderMap>,
     ) -> Result<reqwest::Response, reqwest::Error> {
         // Process the URL and build the request based on the context
         let url = self.base_url.join(path).unwrap();
+
+        // If the DEBUG environment variable is set, print the URL
+        if std::env::var("DEBUG").is_ok() {
+            print_call(url.to_string());
+        }
+
+        let mut headers = header_map.unwrap_or_default();
+
+        if let RequestType::File { file, .. } = &context {
+            let size = file.get_size().await.expect("Failed to get file size");
+
+            if size > 0 {
+                // This is the case in local files, but we cannot use it for
+                // remote or receiver stream files because the size is unknown
+                headers.insert("Content-Length", size.to_string().parse().unwrap());
+            }
+        }
+
         let request = context
-            .to_request(self.client.request(method, url.clone())).await;
+            .to_request(self.client.request(method, url.clone()))
+            .await
+            .expect("Failed to build request");
+
         let request = match parameters {
             Some(parameters) => request.query(&parameters),
             None => request,
         };
 
-        print_call(url.to_string());
-
-        // Add the API token if it exists
-        let request = match &self.api_token {
-            Some(api_token) => request.header("X-Dataverse-key", api_token),
-            None => request,
-        };
-
-        request.send().await
+        request.headers(headers).send().await
     }
 }
 
@@ -114,7 +191,7 @@ where
         Ok(response) => response,
         Err(err) => {
             print_error(err.to_string());
-            panic!();
+            return Err(err.to_string());
         }
     };
 
@@ -131,24 +208,20 @@ where
                     err.to_string().red().bold(),
                     raw_content.red().bold(),
                 )
-                    .to_string(),
+                .to_string(),
             );
             panic!("{}", err.to_string());
         }
     }
 }
 
-fn print_error(error: String) {
-    println!("\n{} {}\n", "Error:".red().bold(), error, );
+pub(crate) fn print_error(error: String) {
+    println!("\n{} {}\n", "Error:".red().bold(), error,);
 }
 
 fn print_call(url: String) {
     if atty::is(Stream::Stdout) {
-        println!(
-            "{}: {}",
-            "Calling".to_string().blue().bold(),
-            url.to_string()
-        );
+        println!("{}: {}", "Calling".to_string().blue().bold(), url);
     }
 }
 
@@ -190,13 +263,11 @@ mod tests {
         let client = BaseClient::new(&MOCK_SERVER.base_url(), None).unwrap();
 
         let _m = MOCK_SERVER.mock(|when, then| {
-            when.method(httpmock::Method::GET).path("/test");
+            when.method(GET).path("/test");
             then.status(200).body("test");
         });
 
-        let response = client
-            .get("test", None, &RequestType::Plain)
-            .await;
+        let response = client.get("test", None, RequestType::Plain, None).await;
         assert!(response.is_ok());
     }
 
@@ -211,13 +282,18 @@ mod tests {
 
         let raw_body = serde_json::to_string(&expected_body).unwrap();
         let mock = MOCK_SERVER.mock(|when, then| {
-            when.method(httpmock::Method::POST).path("/test_json");
+            when.method(POST).path("/test_json");
             then.status(200).json_body(raw_body.clone());
         });
 
         // Act
         let response = client
-            .post("test_json", None, &RequestType::JSON { body: raw_body })
+            .post(
+                "test_json",
+                None,
+                RequestType::JSON { body: raw_body },
+                None,
+            )
             .await;
 
         // Assert
@@ -231,7 +307,7 @@ mod tests {
         let client = BaseClient::new(&MOCK_SERVER.base_url(), None).unwrap();
 
         let mock = MOCK_SERVER.mock(|when, then| {
-            when.method(httpmock::Method::POST).path("/test_multipart");
+            when.method(POST).path("/test_multipart");
             then.status(200).body("test");
         });
 
@@ -254,9 +330,7 @@ mod tests {
         };
 
         // Act
-        let response = client
-            .post("test_multipart", None, &context)
-            .await;
+        let response = client.post("test_multipart", None, context, None).await;
 
         // Assert
         assert!(response.is_ok());
@@ -269,7 +343,7 @@ mod tests {
         let client = BaseClient::new(&MOCK_SERVER.base_url(), None).unwrap();
 
         let mock = MOCK_SERVER.mock(|when, then| {
-            when.method(httpmock::Method::GET)
+            when.method(GET)
                 .path("/test_parameters")
                 .query_param("key1", "value1")
                 .query_param("key2", "value2");
@@ -282,7 +356,7 @@ mod tests {
         ]));
 
         let response = client
-            .get("test_parameters", parameters, &RequestType::Plain)
+            .get("test_parameters", parameters, RequestType::Plain, None)
             .await;
 
         assert!(response.is_ok());
