3f1491ed08d56f259cd2f3c1b68363a69cf23f09
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-04-16 01:19:20 +0200

    implement converters

--- src/identifier.rs
+++ src/identifier.rs
@@ -1,6 +1,8 @@
+use std::fmt::{Display, Formatter};
 use std::str::FromStr;
 
 use serde::{Deserialize, Serialize};
+use crate::data_access::datafile::DataFilePath;
 
 // We differentiate between persistent identifiers and
 // regular identifiers here. This makes it easier to
@@ -30,6 +32,30 @@ impl FromStr for Identifier {
     }
 }
 
+impl Display for Identifier {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self { 
+            Self::Id(id) => write!(f, "{}", id),
+            Self::PersistentId(pid) => write!(f, "{}", pid),
+        }
+    }
+}
+
+impl From<i64> for Identifier {
+    fn from(value: i64) -> Self {
+        Self::Id(value)
+    }
+}
+
+impl From<&DataFilePath> for Identifier {
+    fn from(data_file_path: &DataFilePath) -> Self {
+        match data_file_path {
+            DataFilePath::PersistentID(pid) => Identifier::PersistentId(pid.to_owned()),
+            DataFilePath::DbIdentifier(id) => Identifier::Id(*id),
+            DataFilePath::Path(_) => panic!("Cannot convert DataFilePath::Path to Identifier"),
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -60,6 +86,46 @@ mod tests {
         }
 
         match pid {
+            Identifier::PersistentId(pid) => assert_eq!(pid, "doi:10.5072/FK2/ABC123"),
+            _ => panic!("Expected a persistent id"),
+        }
+    }
+    
+    /// Tests the conversion of an `i64` value to an `Identifier` enum variant.
+    ///
+    /// This test function verifies the `Identifier` enum's ability to correctly convert an `i64` value
+    /// into its `Identifier::Id` variant. The function asserts that the conversion process correctly
+    /// identifies and converts the `i64` value into the appropriate `Identifier` variant.
+    ///
+    /// # Assertions
+    /// - Asserts that an `i64` value is correctly converted to an `Identifier::Id`.
+    #[test]
+    fn test_from_i64() {
+        let id = 123;
+        let id = Identifier::from(id);
+    
+        match id {
+            Identifier::Id(id) => assert_eq!(id, 123),
+            _ => panic!("Expected an id"),
+        }
+    }
+    
+    /// Tests the conversion of a `DataFilePath` enum variant to an `Identifier` enum variant.
+    /// 
+    /// This test function verifies the `Identifier` enum's ability to correctly convert a `DataFilePath`
+    /// enum variant into its corresponding `Identifier` variant. The function asserts that the conversion
+    /// process correctly identifies and converts the `DataFilePath` variant into the appropriate `Identifier`
+    /// variant.
+    /// 
+    /// # Assertions
+    /// - Asserts that a `DataFilePath::PersistentID` variant is correctly converted to an `Identifier::PersistentId`.
+    #[test]
+    fn test_from_data_file_path() {
+        let pid = "doi:10.5072/FK2/ABC123";
+        let data_file_path = DataFilePath::PersistentID(pid.to_owned());
+        let id = Identifier::from(&data_file_path);
+    
+        match id {
             Identifier::PersistentId(pid) => assert_eq!(pid, "doi:10.5072/FK2/ABC123"),
             _ => panic!("Expected a persistent id"),
         }
